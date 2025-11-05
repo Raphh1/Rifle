b95bb089b9c8ef7c958dc39f1684e1f2a769e56b
CesarVictor <cesarvictor@hotmail.fr>
2025-11-05 11:52:27 +0100

    init eslint prettier

---
+++ frontend/.eslintrc.cjs
@@ -0,0 +1,23 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:jsx-a11y/recommended",
+    "prettier",
+  ],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  rules: {},
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+};
